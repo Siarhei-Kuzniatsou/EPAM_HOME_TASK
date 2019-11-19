874b1f0d4d05365f539c55f0e183b5b8c1202058
Siarhei_Kuzniatsou1 <Siarhei_Kuzniatsou1@epam.com>
2019-11-19 14:30:44 +0300

    Refactor WhatDo class

--- homeWork/src/main/java/homeWork2/HomeWork2.java
+++ homeWork/src/main/java/homeWork2/HomeWork2.java
@@ -2,8 +2,8 @@ package homeWork2;
 
 public class HomeWork2 {
     public static void main(String[] args) {
-        WhatDo whatDo = new WhatDo();
-        whatDo.whatDo(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+        WhatDo whatDo = new WhatDo(args);
+
     }
 
 }
--- homeWork/src/main/java/homeWork2/WhatDo.java
+++ homeWork/src/main/java/homeWork2/WhatDo.java
@@ -1,10 +1,19 @@
 package homeWork2;
 
 public class WhatDo {
+    private String[] args;
+
+    WhatDo (String[] args) {
+        this.args = args;
+        whatDo(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+    }
+
     HowFibanachi howfibanachi = new HowFibanachi();
     HowFactarial howFactarial = new HowFactarial();
-    public void whatDo(int num, int method, int parametr) {
+    private void whatDo(int num, int method, int parametr) {
+
         if(num == 1) howfibanachi.howFibanachi(method, parametr);
         if(num == 2) howFactarial.howFactarial(method, parametr);
-    };
+    }
+
 }
