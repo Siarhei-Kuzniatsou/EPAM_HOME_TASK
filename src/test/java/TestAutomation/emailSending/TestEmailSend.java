2d0b446c5d2d7a1ead973bd4451ad9b76dc9bc5e
Siarhei_Kuzniatsou1 <Siarhei_Kuzniatsou1@epam.com>
2019-12-19 19:44:59 +0300

    TestAutomation mail.ru

--- src/test/java/TestAutomation/emailSending/TestEmailSend.java
+++ src/test/java/TestAutomation/emailSending/TestEmailSend.java
@@ -1,29 +1,44 @@
 package TestAutomation.emailSending;
 
+import com.sun.org.apache.xpath.internal.XPath;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.function.Executable;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
 public class TestEmailSend {
-    public static  WebDriver driver;
+
+    @FindBy(
+            how = How.XPATH,
+            using = "//span[contains(text(),'Написать письмо')]")
+    WebElement sendMessage;
+    public static WebDriver driver;
 
     @BeforeAll
-    static void setup() throws InterruptedException {
+    void setup() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:/Users/Siarhei_Kuzniatsou1/Desktop/repos/EPAM_HOME_TASK/src/test/java/TestAutomation/emailSending/chromedriver/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        PageFactory.initElements(driver, this);
+
     }
 
-    @Test
+
     @DisplayName("Login to the mail box")
     @Order(1)
-//    @Disabled
+    @Test
     void login() throws InterruptedException {
         driver.get("http://www.mail.ru/");
         WebElement login = driver.findElement(By.id("mailbox:login"));
@@ -35,40 +50,42 @@ public class TestEmailSend {
         WebElement sentpassword = driver.findElement(By.id("mailbox:password"));
         sentpassword.sendKeys("g1020958G");
         password.submit();
-        WebElement result = driver.findElement(By.id("PH_user-email"));
-        System.out.println(result.getText());
-        System.out.println(result.getAttribute("value"));
-        Assertions.assertEquals("general_ks@mail.ru", result.getText());
-        Assertions.assertEquals("general_ks@mail.ru", result.getAttribute("text"));
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOfAllElements(sendMessage));
+    }
+
+    @DisplayName("Create a new mail (fill addressee, subject and body fields)")
+    @Order(2)
+    @Test
+    void createNewEmail() throws InterruptedException {
+
+//        WebDriverWait wait = new WebDriverWait(driver, 20);
+//        wait.until(ExpectedConditions.visibilityOfAllElements(driver.findElement(By.cssSelector("a.b-toolbar__btn.js-shortcut[data-title*='Написать письмо']"))));
+//        driver.findElement(By.xpath("///div/a/span[contains(text(),'Написать письмо')]")).submit();
+        System.out.println(sendMessage.isEnabled());
+        sendMessage.click();
+        Thread.sleep(2000);
+
+//        driver.findElement(By.id("compose_to")).sendKeys("general_ks@mail.ru");
+//        driver.findElement(By.name("Subject")).sendKeys("Test");
+//        driver.findElement(By.id("tinymce")).sendKeys("Test!");
+//        driver.findElement(By.cssSelector("ata-title='Сохранить'")).submit();
+//        driver.findElement(By.cssSelector("data-text='Сохранить черновик'")).submit();
+//        driver.findElement(By.id("500001")).submit();
+//        Assertions.assertDoesNotThrow((Executable) driver.findElement(By.cssSelector("b-datalist__body.b-datalist__item js-datalist-item.b-datalist__item__body.Test")));
+//        driver.findElement(By.cssSelector("b-datalist__body.b-datalist__item js-datalist-item.b-datalist__item__body.Test")).submit();
+
 
     }
 
-//    @Test
-//    @DisplayName("Assert, that the login is successful")
-//    @Order(2)
-//    @Disabled
-//    void loginIsSuccessful(){
-//
-//    }
-//
-//    @Test
-//    @DisplayName("Create a new mail (fill addressee, subject and body fields)")
-//    @Order(3)
-//    @Disabled
-//    void createNewEmail(){
-//
-//    }
-
-//
-//            4) Save the mail as a draft.
-//            5) Verify, that the mail presents in Drafts.
 //            6) Verify the draft content (addressee, subject and body – should be the same as in 3).
 //            7) Send the mail.
 //8) Verify, that the mail disappeared from Drafts.
 //            9) Verify, that the mail is in Sent.
 //            10) Log off.
+
     @AfterAll
-    static void closeAll(){
+    static void closeAll() {
         driver.quit();
     }
 
